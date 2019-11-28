8f9b313ffc6448525ed92d2f2b23667edf89374f
oggie <ognjen@getpenta.com>
2019-11-28 14:05:13 +0100

    add light and remove mouse event handler, add cubes to try out the light

--- main.c
+++ main.c
@@ -4,26 +4,31 @@
 
 static int window_width, window_height;
 
-static float nX = 0;
-static float nY = 0.5;
-static float nZ = 2;
+static float camPosX = 0;
+static float camPosY = 0.5;
+static float camPosZ = 2;
 
-static float lookingAtX = 0;
-static float lookingAtY = 0.5;
-static float lookingAtZ = 0.5;
+static float camFrontX = 0;
+static float camFrontY = 0.5;
+static float camFrontZ = 1;
 
-static float prev_mouse_x = 0;
-static float prev_mouse_y = 0.5;
+static float camUpX = 0;
+static float camUpY = 1;
+static float camUpZ = 0;
 
-static int should_handle_mouse_movement = 0;
-static int is_first_mouse_movement_handling = 1;
+GLfloat light_ambient[] = { 0.7, 0.7, 0.7, 0.4 };
+GLfloat light_diffuse[] = { 0.4, 0.9, 0.4, 0.4 };
+GLfloat light_specular[] = { 0.2, 0.9, 0.2, 0.2 };
+GLfloat light_position[] = { 0, 0, 1, 0 };
 
 static void on_reshape(int width, int height);
 static void on_display(void);
-static void on_mouse_movement(int x, int y);
 static void on_keyboard(unsigned char key, int x, int y);
-
-void setColorRGB(int r, int g, int b);
+void drawTree(float coordX, float coordY, float coordZ);
+void drawCylinder(float coordX, float coordY, float coordZ, float radius, float height);
+void drawCone(float coordX, float coordY, float coordZ, float radius, float height);
+void setMaterialForGrass();
+void setMaterialForRedCube();
 
 int main(int argc, char **argv)
 {
@@ -32,15 +37,22 @@ int main(int argc, char **argv)
 
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow(argv[0]);
+    glutCreateWindow("Forest run");
 
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
-    glutPassiveMotionFunc(on_mouse_movement);
     glutKeyboardFunc(on_keyboard);
 
-    glClearColor(1, 1, 1, 0);
+    glClearColor(0.098f,0.098f,0.43f, 0);
     glEnable(GL_DEPTH_TEST);
+
+    // Light
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
 
     glutMainLoop();
     return 0;
@@ -49,6 +61,8 @@ int main(int argc, char **argv)
 static void on_display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -60,69 +74,62 @@ static void on_display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-        nX, nY, nZ,                       // where's our camera (eye)
-        lookingAtX, lookingAtY, nZ - 1.5, // what are we looking at
-        0, 1, 0);                         // what's our up vector
+        camPosX, camPosY, camPosZ, // where's our camera (eye)
+        camFrontX, camFrontY, camFrontZ, // what are we looking at
+        camUpX, camUpY, camUpZ); // what's our up vector
+
+    // Cube 1
+    glPushMatrix();
+        setMaterialForRedCube();
+        glTranslatef(0, 0.15, 0);
+        glutSolidCube(0.3);
+    glPopMatrix();
+
+    // Cube 2
+    glPushMatrix();
+        setMaterialForRedCube();
+        glTranslatef(0.3, 0.1, -0.3);
+        glutSolidCube(0.2);
+    glPopMatrix();
 
     // Grass
-    setColorRGB(11,102,35);
-    glBegin(GL_QUADS);
-        glPushMatrix();
-            glVertex3f(3.0f, 0, -3.0f);
-            glVertex3f(-3.0f, 0, -3.0f);
-            glVertex3f(-3.0f, 0, 3.0f);
-            glVertex3f(3.0f, 0, 3.0f);
-        glPopMatrix();
-    glEnd();
+    glPushMatrix();
+        setMaterialForGrass();
+        glScalef(10, 0.01, 10);
+        glutSolidCube(1);
+    glPopMatrix();
 
     // Refresh
     glutSwapBuffers();
 }
 
-void setColorRGB(int r, int g, int b)
-{
-    glColor3f(((float)r) / 255, ((float)g) / 255, ((float)b) / 255);
-}
-
-static void on_mouse_movement(int x, int y)
-{
-    if (should_handle_mouse_movement)
-    {
-        if ((float)window_width - (float)x < (float)window_width / 50) // poslednjih 2% ekrana (desna ivica)
-        {
-            lookingAtX += 80 / ((float)window_width);
-        }
-        if ((float)x < (float)window_width / 50) // poslednjih 2% ekrana (leva ivica)
-        {
-            lookingAtX -= 80 / ((float)window_width);
-        }
-        lookingAtX -= 2 * ((float)prev_mouse_x - (float)x) / ((float)window_width);
-        lookingAtY += 2 * ((float)prev_mouse_y - (float)y) / ((float)window_height);
-    }
-    prev_mouse_x = (float)x;
-    prev_mouse_y = (float)y;
-    glutPostRedisplay();
-}
-
 static void on_keyboard(unsigned char key, int x, int y)
 {
+    float angle = 0.05f;
     switch (key)
     {
     case 27:
         exit(0);
         break;
     case 'w':
-        nX += lookingAtX * 0.1f;
-        // TODO dodaj y deo
-        nZ -= 0.1f;
+        camPosZ -= 0.05f;
         break;
     case 's':
-        nX -= lookingAtX * 0.1f;
-        // TODO dodaj y deo
-        nZ += 0.1f;
+        camPosZ += 0.05f;
         break;
-    case 'z':
-        should_handle_mouse_movement = 1;
+    case 'a':
+        camPosX -= 0.05f;
+        camFrontX -= 0.05f;
+        break;
+    case 'd':
+        camPosX += 0.05f;
+        camFrontX += 0.05f;
+        break;
+    case '.':
+        camFrontX -= sin(angle);
+        break;
+    case '/':
+        camFrontX += sin(angle);
         break;
     }
     // force rerender
@@ -133,4 +140,20 @@ static void on_reshape(int width, int height)
 {
     window_width = width;
     window_height = height;
+}
+
+void setMaterialForGrass() {
+    GLfloat ambient_coeffs[] = { 0.05, 0.4, 0.05, 0.2 };
+    GLfloat diffuse_coeffs[] = { 0.1, 0.9, 0.1, 0.9 };
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glutPostRedisplay();
+}
+
+void setMaterialForRedCube() {
+    GLfloat ambient_coeffs[] = { 0.9, 0.05, 0.05, 0.2 };
+    GLfloat diffuse_coeffs[] = { 0.9, 0.05, 0.05, 0.9 };
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glutPostRedisplay();
 }
